b36dd6ee2c82965c85d6fe77446382a748f62490
Tim Bachmann <tim.bachmann@stud.unibas.ch>
2018-11-14 10:08:57 +0100

    minor changes to aufg 04

--- exercise/04/Aufg1b/1b.asm
+++ exercise/04/Aufg1b/1b.asm
@@ -8,20 +8,23 @@
 ldi r16, $8  ; Load imput value
  
 
-ldi r17, 0 ; Load value 12
+ldi r17, 0 ; Load value 0
 ldi r18, $0  ; initialize sum variable to 0
 
 loop:
 add r18, r16 ; Add input value to sum
 inc r17 ; decrement the counter value
 
-cpi r17, 12   ; compare the counter value to 12 (end of loop)
+cpi r17, 14   ; compare the counter value to 12 (end of loop)
 brne loop    ; if the counter value is not 12 continue with loop
+
 
 ldi r19, 255
 eor r18, r19 ; vorzeichen wechseln wie in den slides
 
 end:
 jmp end
+
+
 
 
